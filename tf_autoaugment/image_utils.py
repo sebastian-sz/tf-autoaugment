f8464e7c78760c41fb7b4cc0e1b574adc3313d22
sebastian-sz <mocart15@gmail.com>
2021-05-06 22:45:42 +0200

    Large refactor. Added float and int32 compatibility and consistency checks.

--- tf_autoaugment/image_utils.py
+++ tf_autoaugment/image_utils.py
@@ -1,11 +1,8 @@
-"""Contains utility scripts."""
-
 import tensorflow as tf
 
 
 def wrap_batch(image):
-    """
-    Return images batch with an extra channel set to all 1s.
+    """Return images batch with an extra channel set to all 1s.
 
     This is a modified version of the script present in the original repository:
     https://github.com/tensorflow/tpu/blob/0b33da3bdfc3706666dfa25631e0eed76a111e07/models/official/efficientnet/autoaugment.py#L404  # noqa: E501
@@ -42,6 +39,7 @@ def unwrap_batch(image, replace):
 
     # Find all pixels where the last channel is zero.
     alpha_channel = flattened_image[:, 3]
+    replace = tf.cast(replace, image.dtype)
 
     replace = tf.concat([replace, tf.ones([1], image.dtype)], 0)
 
@@ -59,54 +57,44 @@ def unwrap_batch(image, replace):
     return image
 
 
-def wrap(image):
+def blend_batch(image1: tf.Tensor, image2: tf.Tensor, factor: float) -> tf.Tensor:
+    """Blend image1 and image2 using 'factor'.
+
+    Factor can be above 0.0.  A value of 0.0 means only image1 is used.
+    A value of 1.0 means only image2 is used.  A value between 0.0 and
+    1.0 means we linearly interpolate the pixel values between the two
+    images.  A value greater than 1.0 "extrapolates" the difference
+    between the two pixel values, and we clip the results to values
+    between 0 and 255.
+
+    :param image1: images Tensor of shape (batch, height, width, channels)
+    :param image2: images Tensor of shape (batch, height, width, channels)
+    :param factor A floating point value above 0.0.
+    :return tf.Tensor. A blended image.
     """
-    Return 'image' with an extra channel set to all 1s.
+    assert image1.dtype == image2.dtype, "Both images need to be the same dtype."
+    original_dtype = image1.dtype
 
-    Source:
-    https://github.com/tensorflow/tu/blob/0b33da3bdfc3706666dfa25631e0eed76a111e07/models/official/efficientnet/autoaugment.py#L404
-    """
-    shape = tf.shape(image)
-    extended_channel = tf.ones([shape[0], shape[1], 1], image.dtype)
-    extended = tf.concat([image, extended_channel], 2)
-    return extended
+    if factor == 0.0:
+        return image1
+    if factor == 1.0:
+        return image2
 
+    image1 = tf.cast(image1, tf.float32)
+    image2 = tf.cast(image2, tf.float32)
 
-def unwrap(image, replace):
-    """
-    Unwraps an image produced by wrap.
+    difference = image2 - image1
+    scaled = factor * difference
 
-    Where there is a 0 in the last channel for every spatial position,
-    the rest of the three channels in that spatial dimension are grayed
-    (set to 128).  Operations like translate and shear on a wrapped
-    Tensor will leave 0s in empty locations.  Some transformations look
-    at the intensity of values to do preprocessing, and we want these
-    empty pixels to assume the 'average' value, rather than pure black.
-    Args:
-      image: A 3D Image Tensor with 4 channels.
-      replace: A one or three value 1D tensor to fill empty pixels.
-    Returns:
-      image: A 3D image Tensor with 3 channels.
+    temp = image1 + scaled
 
-    Source:
-        https://github.com/tensorflow/tpu/blob/0b33da3bdfc3706666dfa25631e0eed76a111e07/models/official/efficientnet/autoaugment.py#L412 # noqa: E501
-    """
-    image_shape = tf.shape(image)
-    # Flatten the spatial dimensions.
-    flattened_image = tf.reshape(image, [-1, image_shape[2]])
+    # Interpolate
+    if factor > 0.0 and factor < 1.0:
+        # Interpolation means we always stay within 0 and 255.
+        result = tf.cast(temp, tf.uint8)
+    else:
+        # Extrapolate:
+        # We need to clip and then cast.
+        result = tf.cast(tf.clip_by_value(temp, 0.0, 255.0), tf.uint8)
 
-    # Find all pixels where the last channel is zero.
-    alpha_channel = flattened_image[:, 3]
-
-    replace = tf.concat([replace, tf.ones([1], image.dtype)], 0)
-
-    # Where they are zero, fill them in with 'replace'.
-    flattened_image = tf.where(
-        tf.expand_dims(tf.equal(alpha_channel, 0), axis=-1),  # This had to be changed.
-        tf.ones_like(flattened_image, dtype=image.dtype) * replace,
-        flattened_image,
-    )
-
-    image = tf.reshape(flattened_image, image_shape)
-    image = tf.slice(image, [0, 0, 0], [image_shape[0], image_shape[1], 3])
-    return image
+    return tf.cast(result, original_dtype)
--- tf_autoaugment/tests/test_transforms.py
+++ tf_autoaugment/tests/test_transforms.py
@@ -1,42 +1,96 @@
-"""Contains test to sanity check whether each transform can be executed."""
-
-from random import randint
-
 import numpy as np
-import pytest
 import tensorflow as tf
+from absl.testing import absltest, parameterized
 
-from tf_autoaugment.transforms import NAME_TO_TRANSFORM
+from tf_autoaugment.transforms.transforms import NAME_TO_TRANSFORM
+
+DTYPES_TO_TEST = [tf.int32, tf.uint8, tf.float32]
+TRANSFORMS_TO_TEST = list(NAME_TO_TRANSFORM.keys())
+
+TEST_NAMED_PARAMS = [  # Named params for pretty printing
+    {
+        "testcase_name": f"{transform_name}_{dtype.name}",
+        "transform_name": transform_name,
+        "dtype": dtype,
+    }
+    for transform_name in TRANSFORMS_TO_TEST
+    for dtype in DTYPES_TO_TEST
+]
 
 
-@pytest.mark.parametrize("transform_name", list(NAME_TO_TRANSFORM.keys()))
-def test_eager_transforms(transform_name):
-    magnitude = randint(1, 7)
-    original_frames = tf.random.uniform(
-        (5, 224, 224, 3), minval=0, maxval=255, dtype=tf.int32
-    )  # Todo: add float compatibility.
+class TestTransformsForwardPass(parameterized.TestCase):
+    rng = tf.random.Generator.from_non_deterministic_state()
+    mock_input = rng.uniform(
+        shape=(5, 224, 224, 3), minval=0, maxval=255, dtype=tf.int32
+    )
+    level = int(rng.uniform(shape=(), minval=1, maxval=7, dtype=tf.int32))
 
-    transform = NAME_TO_TRANSFORM[transform_name]
-    transformed_images = transform(original_frames, magnitude=magnitude)
+    @parameterized.named_parameters(TEST_NAMED_PARAMS)
+    def test_transform_eager_mode(self, transform_name: str, dtype: tf.dtypes.DType):
+        inputs = tf.cast(self.mock_input, dtype)
+        transform = NAME_TO_TRANSFORM[transform_name]
 
-    assert transformed_images.shape == original_frames.shape
-    with pytest.raises(AssertionError):
-        np.testing.assert_array_equal(transformed_images, original_frames)
+        outputs = transform(inputs, self.level)
+
+        assert inputs.dtype == outputs.dtype
+        assert inputs.shape == outputs.shape
+        with self.assertRaises(AssertionError):
+            np.testing.assert_array_equal(outputs, inputs)
+
+    @parameterized.named_parameters(TEST_NAMED_PARAMS)
+    def test_transform_graph_mode(self, transform_name: str, dtype: tf.dtypes.DType):
+        inputs = tf.cast(self.mock_input, dtype)
+        dataset = tf.data.Dataset.from_tensor_slices([inputs])
+
+        transform = NAME_TO_TRANSFORM[transform_name]
+        dataset = dataset.map(lambda x: transform(x, self.level))
+        outputs = tf.convert_to_tensor(list(dataset))[0]
+
+        assert inputs.dtype == outputs.dtype
+        assert inputs.shape == outputs.shape
+        with self.assertRaises(AssertionError):
+            np.testing.assert_array_equal(outputs, inputs)
+
+    @parameterized.named_parameters(TEST_NAMED_PARAMS)
+    def test_transform_tf_function(self, transform_name: str, dtype: tf.dtypes.DType):
+        inputs = tf.cast(self.mock_input, dtype)
+        transform = NAME_TO_TRANSFORM[transform_name]
+        transform.__call__ = tf.function(transform.__call__)
+
+        outputs = transform(inputs, self.level)
+
+        assert inputs.dtype == outputs.dtype
+        assert inputs.shape == outputs.shape
+        with self.assertRaises(AssertionError):
+            np.testing.assert_array_equal(outputs, inputs)
 
 
-@pytest.mark.parametrize("transform_name", list(NAME_TO_TRANSFORM.keys()))
-def test_graph_transforms(transform_name):
-    magnitude = randint(1, 7)
-    original_frames = tf.random.uniform(
-        (5, 224, 224, 3), minval=0, maxval=255, dtype=tf.int32
-    )  # Todo: add float compatibility.
-    dataset = tf.data.Dataset.from_tensor_slices([original_frames])
+class TestTransformsOutputConsistency(parameterized.TestCase):
+    rng = tf.random.Generator.from_non_deterministic_state()
+    mock_input = rng.uniform(
+        shape=(5, 224, 224, 3), minval=0, maxval=255, dtype=tf.int32
+    )
+    level = int(rng.uniform(shape=(), minval=1, maxval=7, dtype=tf.int32))
 
-    transform = NAME_TO_TRANSFORM[transform_name]
-    dataset = dataset.map(lambda x: transform(x, magnitude=magnitude))
+    # The transforms were originally designed for uint8
+    mock_input = tf.cast(mock_input, tf.uint8)
+    tolerance = 5  # pixel values for 0-255 range.
 
-    transformed_frames = np.array(list(dataset))[0]
+    @parameterized.named_parameters(TEST_NAMED_PARAMS)
+    def test_output_is_consistent_between_dtypes(
+        self, transform_name: str, dtype: tf.dtypes.DType
+    ):
+        transform = NAME_TO_TRANSFORM[transform_name]
+        expected_outputs = transform._debug_apply(self.mock_input, self.level)  # uint8
 
-    assert transformed_frames.shape == original_frames.shape
-    with pytest.raises(AssertionError):
-        np.testing.assert_array_equal(transformed_frames, original_frames)
+        inputs = tf.cast(self.mock_input, dtype)
+        outputs = transform._debug_apply(inputs, self.level)
+        outputs = tf.cast(outputs, tf.uint8)
+
+        np.testing.assert_allclose(
+            outputs, expected_outputs, rtol=self.tolerance, atol=self.tolerance
+        )
+
+
+if __name__ == "__main__":
+    absltest.main()
